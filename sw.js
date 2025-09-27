51528664104e830a14851a79b9535c0872833c23
EPS-BARLETTA <j.barletta@vauban.lu>
2025-09-27 23:17:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Minimal service worker to replace old ones.
+// Minimal service worker to replace old ones and do nothing else.
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', event => { /* no caching */ });
